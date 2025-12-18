9eb94e9c6795ecf8a94434fa73c27857aae46b4c
Negrita1710 <agustinacardoso707@gmail.com>
2025-12-18 18:02:09 -0300

    añadi la boleta pista

---
+++ funcion/accion/boleta_pista/boleta_pista.php
@@ -0,0 +1,49 @@
+<?php
+
+    session_start();
+
+    error_reporting(E_ALL); 
+    ini_set('display_errors', 1);
+
+
+    require_once  '../../percistencia/boletapista.php';
+    require_once  '../../percistencia/clientes.php';
+
+    if (isset($_GET['buscar'])) {
+        
+        $busqueda = trim($_GET['buscar']);
+                
+    } else {
+        $boletas = BoletaPista::recuperarTodos();
+        $resultados = $boletas;
+    }
+?>
+<div class="h2">
+    <h2>Resultados de la busqueda:</h2>
+</div>
+<div id="inresultado">
+    <table class="table">
+    <tr>
+          <th>ID</th>
+          <th>Moneda</th>
+          <th>Fecha</th>
+          <th>Cliente</th>
+          <th>Accion</th>
+
+    </tr>
+            <?php foreach ($resultados as $boleta): ?>
+              <tr>
+                 <?php
+                    $cliente = Clientes::buscarPorId($boleta['id_cliente']);
+                 ?>
+                <td><?php echo htmlspecialchars($boleta['id']); ?></td>
+                <td><?php echo htmlspecialchars($boleta['moneda']); ?></td>
+                <td><?php echo htmlspecialchars($boleta['fecha']); ?></td>
+                <td><?php echo htmlspecialchars($cliente->getNombre() . " " . $cliente->getApellido()); ?></td>
+                <td onclick="agregarboleta(<?php echo htmlspecialchars($boleta['id']); ?>)">
+                <i class="fa-solid fa-pencil"></i> </td>
+               </tr>
+             <?php endforeach; ?>
+   
+    </table>
+</div>
